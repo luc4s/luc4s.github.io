67d96c34a100347a57a6e9cb6fe0390b99736026
Monnin Lucas <lmo@lerepuis.ch>
2019-12-06 14:09:34 +0100

    Added triangles background

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var camera = new THREE.PerspectiveCamera(
 
 camera.position.z = 5;
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({
+	antialias: true,
+});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -16,16 +18,124 @@ window.requestAnimationFrame = window.requestAnimationFrame
 															|| window.webkitRequestAnimationFrame
 															|| window.msRequestAnimationFrame;
 
-scene.add(new THREE.AmbientLight( 0x404040 ));
+scene.add(new THREE.AmbientLight( 0x202020 ));
 
-var mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1));
-scene.add(mesh);
+var pLight = new THREE.PointLight(0x440000, 16, 3);
+pLight.position.set(1.5, 1, 2);
+scene.add(pLight)
+pLight = new THREE.PointLight(0x004400, 16, 3);
+pLight.position.set(-1.5, 1, 2);
+scene.add(pLight)
+
+var light = new THREE.DirectionalLight(0xFFFFFF, 0.3);
+light.position.set(0, 2, 5);
+scene.add(light);
+
+var light = new THREE.DirectionalLight(0xBD48FF, 0.2);
+light.position.set(0, -5, 5);
+light.target.position.set(0, 0, -5);
+scene.add(light);
+
+var geom = new THREE.BoxGeometry(1, 1, 1);
+
+var material = new THREE.MeshStandardMaterial({
+	color: '#FFFFFF',
+	emissive: 0x444444,
+	roughness: 0.2,
+	metalness: 0,
+});
+
+var group = new THREE.Group();
+
+var mesh = new THREE.Mesh(geom, material);
+mesh.position.x = -3;
+group.add(mesh);
+
+mesh = new THREE.Mesh(geom, material);
+group.add(mesh);
+
+mesh = new THREE.Mesh(geom, material);
+mesh.position.x = 3;
+group.add(mesh);
+
+group.position.y = -1;
+scene.add(group);
+
+// Generate random triangulation
+var m =  new THREE.MeshStandardMaterial({
+	color: 0xFFFFFF,
+	side: THREE.DoubleSide,
+	flatShading: true,
+	roughness: 0.5,
+	metalness: 0.2,
+});
+
+var size = 4;
+
+var vertices = new Float32Array(size * size * 4 * 3 * 6);
+var indices = [];
+function generateTriangulation() {
+	var geom = new THREE.BufferGeometry();	
+	var array = [];
+	var nArray = [];
+	var s = 2;
+	var c = 0;
+	function newVx(x, y) {
+		//var z = perlin((x + 32) / s, (y + 32) / s) - 0.5;
+		//console.log(z);
+		indices[c / 3] = [x, y];
+		vertices[c++] = x;
+		vertices[c++] = y;
+		vertices[c++] = 0;
+	}
+	for (var i = -size; i < size; ++i) {
+		for (var j = -size; j < size; ++j) {
+				newVx(i, j);
+				newVx(i, j + 1);
+				newVx(i + 1, j + 1);
+
+				newVx(i + 1, j + 1);
+				newVx(i + 1, j);
+				newVx(i, j);
+		}
+	}
+	geom.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+	geom.computeVertexNormals();
+	var mesh = new THREE.Mesh(geom, m);
+	mesh.scale.setScalar(2);
+	mesh.position.z = -3;
+	return mesh;
+}
+
+var back = generateTriangulation();
+scene.add(back);
+
+function ondulate() {
+	var length = size * 2;
+	for (var i = 2; i < vertices.length; i += 3) {
+		var vIdx = Math.floor(i / 3);
+		//var x = Math.floor(vIdx / length) % length;
+		//var y = (vIdx - x * length) % length;
+		//console.log(vIdx, indices.length);
+		var x = indices[vIdx][0] + size;
+		var y = indices[vIdx][1] + size;
+		vertices[i] = perlin(x / 2, y / 2) - 0.5;
+	}
+	back.geometry.getAttribute('position').needsUpdate = true;
+	back.geometry.computeVertexNormals();
+}
+ondulate();
+perturbGradient();
+
 
 function tick(timestamp) {
   renderer.render(scene, camera);
-  window.setTimeout(requestAnimationFrame, 13, tick);
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+  perturbGradient();
+  ondulate();
+  //window.setTimeout(requestAnimationFrame, 13, tick);
+  //back.rotation.x += 0.01;
+  //back.rotation.y += 0.02;
+  window.requestAnimationFrame(tick);
 }
-
-requestAnimationFrame(tick);
+renderer.render(scene, camera);
+tick();
---
+++ perlin.js
@@ -0,0 +1,73 @@
+ // Function to linearly interpolate between a0 and a1
+ // Weight w should be in the range [0.0, 1.0]
+ function lerp(a0, a1, w) {
+     return (1.0 - w)*a0 + w*a1;
+ }
+ 
+ var gradient = [];
+
+ // Computes the dot product of the distance and gradient vectors.
+ function dotGridGradient(ix, iy, x, y) {
+     // Compute the distance vector
+     var dx = x - ix;
+     var dy = y - iy;
+
+     // Compute the dot-product
+     return (dx*gradient[iy][ix][0] + dy*gradient[iy][ix][1]);
+ }
+ 
+ // Compute Perlin noise at coordinates x, y
+ function perlin(x, y) {
+ 
+     // Determine grid cell coordinates
+     var x0 = Math.floor(x);
+     var x1 = x0 + 1;
+     var y0 = Math.floor(y);
+     var y1 = y0 + 1;
+ 
+     // Determine interpolation weights
+     // Could also use higher order polynomial/s-curve here
+     var sx = x - x0;
+     var sy = y - y0;
+ 
+     // Interpolate between grid point gradients
+     var n0 = dotGridGradient(x0, y0, x, y);
+     var n1 = dotGridGradient(x1, y0, x, y);
+     var ix0 = lerp(n0, n1, sx);
+     var n0 = dotGridGradient(x0, y1, x, y);
+     var n1 = dotGridGradient(x1, y1, x, y);
+     var ix1 = lerp(n0, n1, sx);
+     var value = lerp(ix0, ix1, sy);
+ 
+     return value;
+ }
+
+// Perturb gradient
+function perturbGradient() {
+     for (var i = 0; i < 128; ++i) {
+          for (var j = 0; j < 128; ++j) {
+               var angle = Math.random() * 2 * Math.PI * 0.01;
+               //console.log(angle);
+               var sin = Math.sin(angle);
+               var cos = Math.cos(angle);
+
+               var x0 = gradient[i][j][0];
+               var y0 = gradient[i][j][1];
+               var x = x0 + (x0 * cos - y0 * sin);
+               var y = y0 + (x0 * sin - y0 * cos);
+               var l = Math.sqrt((x * x) + (y * y));
+               gradient[i][j] = [x/l, y/l];
+          }
+     }
+}
+
+// Init gradient
+for (var i = 0; i < 128; ++i) {
+     gradient[i] = [];
+     for (var j = 0; j < 128; ++j) {
+          var x = Math.random();
+          var y = Math.random();
+          var l = Math.sqrt((x * x) + (y * y));
+          gradient[i][j] = [x / l, y / l];
+     }
+}
