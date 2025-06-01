d8c2fd968419bbd3696cc2cc5528a600b9e2cb52
Lucas Monnin <monnin@itis.swiss>
2025-06-02 00:53:53 +0200

    update config

--- app/luc4s.github.io/next.config.ts
+++ app/luc4s.github.io/next.config.ts
@@ -1,7 +1,9 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-    // output: 'export'
+  output: 'export',
+  reactStrictMode: true,
+  images: {unoptimized: true}
 };
 
 export default nextConfig;
