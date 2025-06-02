0402dcdfcaa3335e2b55f9d97619aae64190322a
Lucas Monnin <monnin@itis.swiss>
2025-06-02 12:40:55 +0200

    use build output

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  images: {unoptimized: true}
+  images: {unoptimized: true},
+  distDir: 'build'
 };
 
 export default nextConfig;
