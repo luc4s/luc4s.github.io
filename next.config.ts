fe6ea3e45acabac2fa00d5d3bfdc7b0692db0a97
Lucas Monnin <monnin@itis.swiss>
2025-06-02 12:17:49 +0200

    Remove unrecognized option (??)

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  distDir: 'dist',
-  images: {unoptimized: true}
+  distDir: 'dist'
 };
 
 export default nextConfig;
