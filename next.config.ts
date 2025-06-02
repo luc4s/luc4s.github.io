5479597fb6b119808a79f3da922ca5a140d4c87f
Lucas Monnin <monnin@itis.swiss>
2025-06-02 12:28:47 +0200

    Use default out/

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   reactStrictMode: true,
-  distDir: 'dist'
+  images: {unoptimized: true}
 };
 
 export default nextConfig;
